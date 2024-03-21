81f6078f2d69e7855f5a94e746a7c315298d8787
Satyajeet Sau <work.satyajeetsau@gmail.com>
2024-03-21 23:33:37 +0530

    Time: 1 ms (9.84%), Space: 44.3 MB (54.38%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,39 +14,26 @@ class Node {
 */
 
 class Solution {
-    private int getIndex(Node head, Node target){
-        int i = 0;
-        while(head!=null && head != target){
-            head = head.next;
-            i++;
-        }
-        return i;
-    }
-    private Node getNode(Node head, int index){
-        int i = 0;
-        while(i < index){
-            head = head.next;
-            i++;
-        }
-        return head;
-    }
     public Node copyRandomList(Node head) {
-        if(head == null) return null;
-        ArrayList<Integer> rl = new ArrayList<>();
-        Node ptr1 = head;
-        Node newHead = new Node(0), ptr2 = newHead;
-        while(ptr1 != null){
-            if(ptr1.random == null) rl.add(null);
-            else rl.add(getIndex(head, ptr1.random));
-            ptr2.next = new Node(ptr1.val);
-            ptr1 = ptr1.next;
-            ptr2 = ptr2.next;
+        Node curr = head;
+        Node newHead = new Node(0), copy = newHead;
+        HashMap<Node,Integer> og = new HashMap<>();
+        HashMap<Integer,Node> cp = new HashMap<>();
+        int index = 0;
+        while(curr != null){
+            copy.next = new Node(curr.val);
+            copy = copy.next;
+            og.put(curr,index);
+            cp.put(index++,copy);
+            curr = curr.next;
         }
-        ptr2 = newHead.next;
-        for(int i=0; i<rl.size(); i++){
-            if(rl.get(i) == null) ptr2.random = null;
-            else ptr2.random = getNode(newHead.next,rl.get(i));
-            ptr2 = ptr2.next;
+        curr = head;
+        copy = newHead.next;
+        while(curr != null){
+            if(curr.random == null) copy.random = null;
+            else copy.random = cp.get(og.get(curr.random));
+            curr = curr.next;
+            copy = copy.next;
         }
         return newHead.next;
     }
